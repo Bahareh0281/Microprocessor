9e74199168cec01c3ba6d7c3ed7466b3d7b189e2
Bahareh Kavousi nejad <107205773+Bahareh0281@users.noreply.github.com>
2024-02-25 14:59:38 +0330

    Update main.asm

--- Session 1/Session 1 - Assembly Codes/Session 1 - Assembly Codes/main.asm
+++ Session 1/Session 1 - Assembly Codes/Session 1 - Assembly Codes/main.asm
@@ -5,42 +5,30 @@
 ; Author : Kavousinejad
 ;
 .include "m32def.inc"
-    LDI      R16,HIGH(RAMEND)
-    OUT      SPH,R16
-    LDI      R16,LOW(RAMEND)
-    OUT      SPL,R16
 
-    LDI      R16,0xFF
-    OUT      DDRB,R16
-L4: LDI      R16,0b00000001
+    LDI      R16, 0xFF
+    OUT      DDRB, R16
+
+L4: LDI      R16, 0b00000001
 
 START:
-    OUT      PORTB,R16
+    OUT      PORTB, R16
     CALL     DELAY
     LSL      R16
-    CPI      R16,0
-    BRNE     START
-
-L5: LDI      R16,0b10000000
+    BRCC     REVERSE
+    RJMP     START
 
 REVERSE:
-    OUT      PORTB,R16
+    LDI      R16, 0b10000000
+    OUT      PORTB, R16
     CALL     DELAY
     LSR      R16
     BRNE     REVERSE
-    JMP      L4
     RJMP     L4
 
 DELAY:
-    LDI      R17,0xFF
-L1: IN       R18,PINA
-L2: LDI      R19,0x10
-L3: DEC      R19
-    BRNE     L3
-    DEC      R18
-    BRNE     L2
-    DEC      R17
+    LDI      R17, 0xFF
+L1: DEC      R17
     BRNE     L1
-    RET  
-
+    RET
 
