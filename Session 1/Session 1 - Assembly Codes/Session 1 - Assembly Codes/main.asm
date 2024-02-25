f6311482c67e726c5f675c1a1b879a93f25940eb
Bahareh Kavousi nejad <107205773+Bahareh0281@users.noreply.github.com>
2024-02-25 15:13:12 +0330

    MP LAB Session 1 Completed

--- Session 1/Session 1 - Assembly Codes/Session 1 - Assembly Codes/main.asm
+++ Session 1/Session 1 - Assembly Codes/Session 1 - Assembly Codes/main.asm
@@ -1,34 +1,42 @@
-;
-; Session 1 - Assembly Codes.asm
-;
-; Created: 2/25/2024 2:48:31 PM
-; Author : Kavousinejad
-;
+; Bahareh Kavousi nejad
+; 99431217
 .include "m32def.inc"
+    LDI      R16,HIGH(RAMEND)
+    OUT      SPH,R16
+    LDI      R16,LOW(RAMEND)
+    OUT      SPL,R16
 
-    LDI      R16, 0xFF
-    OUT      DDRB, R16
+    LDI      R16,0xFF
+    OUT      DDRB,R16
+L4: LDI      R16,0b00000001
 
-L4: LDI      R16, 0b00000001
-
-START:
-    OUT      PORTB, R16
-    CALL     DELAY
+BEGIN:
+    OUT      PORTB,R16
+    CALL     DELAYS
     LSL      R16
-    BRCC     REVERSE
-    RJMP     START
+    CPI      R16,0
+    BRNE     BEGIN
 
-REVERSE:
-    LDI      R16, 0b10000000
-    OUT      PORTB, R16
-    CALL     DELAY
+L5: LDI      R16,0b10000000
+
+END:
+    OUT      PORTB,R16
+    CALL     DELAYS
     LSR      R16
-    BRNE     REVERSE
+    BRNE     END
+    JMP      L4
     RJMP     L4
 
-DELAY:
-    LDI      R17, 0xFF
-L1: DEC      R17
+DELAYS:
+    LDI      R17,0xFF
+L1: IN       R18,PINA
+L2: LDI      R19,0x10
+L3: DEC      R19
+    BRNE     L3
+    DEC      R18
+    BRNE     L2
+    DEC      R17
     BRNE     L1
-    RET
+    RET  
+
 
